57dafc6ff2e46b3794bc2a738d01a33975fa3baf
JessadaInta <nnicenicekung@gmail.com>
2025-01-28 12:06:24 +0700

    comit2

---
+++ lab5/01-variables-data-types/01-console/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World.");
+console.log(12.9 + 78);
+console.log("ans"+ 78+12 );
+
+const cat="kiko";
+let fumo;
+fumo="fumofumo";
+
+
+console.log(fumo);
+console.log(cat);
+
+console.warn('this is for my salt')
