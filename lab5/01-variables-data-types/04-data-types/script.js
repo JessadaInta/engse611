d348971df51e83ff8ed4c5055369058a679089bf
JessadaInta <nnicenicekung@gmail.com>
2025-03-03 19:53:14 +0700

    C1

---
+++ lab5/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+let c = 'hello world';
+let d = '2';
+
+
+
+console.log(a,b,c);
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(d, typeof d);
