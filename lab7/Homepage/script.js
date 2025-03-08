1d9fcea149b7004726039894a943f7222019f61d
JessadaInta <nnicenicekung@gmail.com>
2025-03-08 10:13:01 +0700

    C4

--- lab7/Homepage/script.js
+++ lab7/Homepage/script.js
@@ -1,5 +1,20 @@
 let img = document.getElementById("zoom");
 
-        img.onclick = function() {
-            img.classList.toggle("azoom"); 
-        }
+img.onclick = function () {
+    img.classList.toggle("azoom");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let buttons = document.querySelectorAll("table.table-show button");
+
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", function () {
+            // เปลี่ยนเส้นทางไปยังหน้าเว็บที่ต้องการ (เช่น challenge1.html, challenge2.html, ...)
+            window.location.href = `../challengeFile/challenge${index + 1}.html`;
+        });
+    });
+});
+
+
